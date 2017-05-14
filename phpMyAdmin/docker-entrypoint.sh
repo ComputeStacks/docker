d07d00b6288879e02b77e2039f3385aaec8732b3
Kris Watson <kris@computestacks.com>
2017-05-14 15:55:06 -0700

    Update phpMyAdmin

--- phpMyAdmin/docker-entrypoint.sh
+++ phpMyAdmin/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -f "/var/www/html/config.inc.php" ]; then
   cd /var/www/html
   rm config.sample.inc.php
-  wget -O /var/www/html/config.inc.php $BASE_URL/stacks/v2/assets/pma?h=$HOSTNAME
+  wget -O /var/www/html/config.inc.php $BASE_URL/$API_URL?h=$HOSTNAME
   chown www-data:www-data /var/www/html/config.inc.php
   chmod 640 /var/www/html/config.inc.php
 fi
