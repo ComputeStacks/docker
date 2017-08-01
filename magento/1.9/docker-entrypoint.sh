a8620b26d92d1d0eacabc42d366b730ae68e05c4
Kris Watson <kris@computestacks.com>
2017-08-01 10:05:16 -0700

    Fix Magento 1.9 and upgrade to 1.9.3.4

--- magento/1.9/docker-entrypoint.sh
+++ magento/1.9/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
 if [ ! -d "/var/www/html/magento" ]; then
-  mysql -h $MAGENTO_MYSQL_HOST -u$MAGENTO_MYSQL_USER -p$MAGENTO_MYSQL_PASS -e "CREATE DATABASE IF NOT EXISTS ${MAGENTO_MYSQL_DBNAME};"
+  sleep 3
+  mysql -h $MAGENTO_MYSQL_HOST -u $MAGENTO_MYSQL_USER -p $MAGENTO_MYSQL_PASS -e "CREATE DATABASE IF NOT EXISTS ${MAGENTO_MYSQL_DBNAME};"
   mv /usr/src/magento /var/www/html/
   chown -R www-data:www-data /var/www/html
   sed -i 's#CACHE_SERVER_HOST#'"$MEMCACHE_HOST"'#' /var/www/html/magento/app/etc/mage-cache.xml
