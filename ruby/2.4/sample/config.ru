dd59b241527e05e4d8b7e3b036c5a5ae95bfacbc
Kris Watson <kris@computestacks.com>
2018-09-05 13:08:27 -0700

    Updated sample app for ruby

--- ruby/2.4/sample/config.ru
+++ ruby/2.4/sample/config.ru
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class SampleApp < Sinatra::Base
 
-  get('/') { 'Ruby Docker Container' }
+  get('/') { '<h4>Ruby Docker Container</h4><p><a href="https://www.notion.so/computestacks/Ruby-Container-8bd2aaab3c7d4d7fa8bb86c83d56b518" target="_blank">Click here</a> to view documentation on how to use this container.</p>' }
 
 end
 
--- ruby/2.5/entrypoint.sh
+++ ruby/2.5/entrypoint.sh
@@ -5,9 +5,13 @@ if ! [ "$(ls -A)" ]; then
   echo >&2 "No files found in $PWD - copying sample files..."
   tar cf - --one-file-system -C /usr/src/sample . | tar xf -
   echo >&2 "Complete! Sample files have been successfully copied to $PWD"
+  echo >&2 "Running bundler.."
+  bundle
   chown -R www-data:www-data /var/www
 else
-  echo >&2 "Files exist in volume, will not move sample files.."
+  echo >&2 "Files exist in volume, will not move sample files."
+  echo >&2 "Running bundler.."
+  bundle
 fi
 
 exec "$@"
