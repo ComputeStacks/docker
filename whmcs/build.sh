7ed536f5d4675076c4e20df6e12fc62a04159c26
Kris Watson <kris@computestacks.com>
2020-07-14 09:36:19 -0700

    Update WHMCS

--- whmcs/build.sh
+++ whmcs/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-docker build -t cmptstks/whmcs:php7.2-litespeed litespeed/php7.2
+set -e
 
-docker tag cmptstks/whmcs:php7.2-litespeed cmptstks/whmcs:latest
+docker build --no-cache -t cmptstks/whmcs:php7.2-litespeed litespeed/php7.2
+docker build --no-cache -t cmptstks/whmcs:php7.3-litespeed litespeed/php7.3
+
+docker tag cmptstks/whmcs:php7.3-litespeed cmptstks/whmcs:latest
 
 docker push cmptstks/whmcs:php7.2-litespeed
+docker push cmptstks/whmcs:php7.3-litespeed
 docker push cmptstks/whmcs:latest
