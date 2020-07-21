2d6e7d6c5ccf03f53bc2642cd3f642d9ad6bd968
Kris Watson <kris@computestacks.com>
2020-07-20 17:03:09 -0700

    Update whmcs and remove php 7.2 from build script

--- whmcs/build.sh
+++ whmcs/build.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-docker build --no-cache -t cmptstks/whmcs:php7.2-litespeed litespeed/php7.2
 docker build --no-cache -t cmptstks/whmcs:php7.3-litespeed litespeed/php7.3
 
 docker tag cmptstks/whmcs:php7.3-litespeed cmptstks/whmcs:latest
 
-docker push cmptstks/whmcs:php7.2-litespeed
 docker push cmptstks/whmcs:php7.3-litespeed
 docker push cmptstks/whmcs:latest
