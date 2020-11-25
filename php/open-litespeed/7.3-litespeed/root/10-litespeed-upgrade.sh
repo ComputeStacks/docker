5577c59b5b82ceb1b6d174c12edd57f816184add
Kris Watson <kris@computestacks.com>
2020-11-24 22:15:06 -0800

    php litespeed will now update on restart

--- php/open-litespeed/7.3-litespeed/root/10-litespeed-upgrade.sh
+++ php/open-litespeed/7.3-litespeed/root/10-litespeed-upgrade.sh
@@ -27,7 +27,27 @@ if [ $CURRENT_VERSION != $NEW_VERSION ]; then
   if [ -f /usr/src/openlitespeed_${LS_VERSION}_amd64.deb ]; then
     dpkg -i /usr/src/openlitespeed_${LS_VERSION}_amd64.deb
   else
-    echo "Fatal error, /usr/src/openlitespeed_${LS_VERSION}_amd64.deb"
+    echo "Saving current configuration..."
+    mkdir -p /root/.lsws_snapshot
+    if [ -f /usr/local/lsws/admin/conf/admin_config.conf ]; then
+      mv /usr/local/lsws/admin/conf/admin_config.conf /root/.lsws_snapshot/
+    fi
+    if [ -f /usr/local/lsws/admin/conf/htpasswd ]; then
+      mv /usr/local/lsws/admin/conf/htpasswd /root/.lsws_snapshot/
+    fi
+    if [ -d /usr/local/lsws/conf ]; then
+      mv /usr/local/lsws/conf /root/.lsws_snapshot/
+    fi
+    echo "Installing new version..."
+    rm -rf /usr/local/lsws/* && mv /usr/src/lsws/* /usr/local/lsws/
+    echo "Restoring previous configuration..."
+    mv /root/.lsws_snapshot/admin_config.conf /usr/local/lsws/admin/conf/
+    mv /root/.lsws_snapshot/htpasswd /usr/local/lsws/admin/conf/
+    rm -rf /usr/local/lsws/conf && mv /root/.lsws_snapshot/conf /usr/local/lsws/
+    chown -R lsadm: /usr/local/lsws/conf/vhosts
+    rm -rf /root/.lsws_snapshot
+    echo "Finished upgrade to $NEW_VERSION"
+    # echo "Fatal error, /usr/src/openlitespeed_${LS_VERSION}_amd64.deb"
   fi
 else
   echo "Installed version matches current version, exiting upgrade process."
--- php/open-litespeed/7.4-litespeed/root/pecl/install_imagick.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/expect
-spawn /usr/local/lsws/lsphp74/bin/pecl install imagick
-
-expect {Please provide the prefix of ImageMagick installation [autodetect]}
-send "\r"
-expect eof
