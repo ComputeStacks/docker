d7c6e1e28a19bb831c200fa11715d989ba1e9fff
Kris Watson <kris@computestacks.com>
2020-12-01 00:43:57 -0800

    Update ssh bastion image

--- ssh-bastion/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker build --no-cache -t cmptstks/ssh:beta .
-
-docker tag cmptstks/ssh:beta cmptstks/ssh:latest
-docker push cmptstks/ssh:beta
-docker push cmptstks/ssh:latest
